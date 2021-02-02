f1f97a429b59ad2da7e5ff7733e21922bea33be4
Hung Hoang <hoanghun5@gmail.com>
2021-02-02 15:39:08 +0100

    Removed redundant array list creation in analysis.

---
+++ analyzer/src/main/java/cz/mxmx/memoryanalyzer/memorywaste/ReferenceWasteAnalyzer.java
@@ -0,0 +1,77 @@
+package cz.mxmx.memoryanalyzer.memorywaste;
+
+import cz.mxmx.memoryanalyzer.model.ClassDump;
+import cz.mxmx.memoryanalyzer.model.InstanceDump;
+import cz.mxmx.memoryanalyzer.model.InstanceFieldDump;
+import cz.mxmx.memoryanalyzer.model.MemoryDump;
+import cz.mxmx.memoryanalyzer.model.memorywaste.ReferencesWaste;
+import cz.mxmx.memoryanalyzer.model.memorywaste.Waste;
+import edu.tufts.eaftan.hprofparser.parser.datastructures.Type;
+import edu.tufts.eaftan.hprofparser.parser.datastructures.Value;
+
+import java.util.*;
+
+public class ReferenceWasteAnalyzer implements WasteAnalyzer {
+
+    @Override
+    public List<Waste> findMemoryWaste(MemoryDump memoryDump) {
+        Map<Long, InstanceDump> userInstances = memoryDump.getUserInstances();
+        long referencesCount = 0;
+        long nullReferencesCount = 0;
+
+        for (InstanceDump instance : userInstances.values()) {
+            ReferenceCounter references = countReferences(instance);
+            referencesCount += references.getReferencesCount();
+            nullReferencesCount += references.getNullReferencesCount();
+        }
+
+        return Collections.singletonList(new ReferencesWaste(this, referencesCount, nullReferencesCount));
+    }
+
+    private ReferenceCounter countReferences(InstanceDump instance) {
+        ClassDump classDump = instance.getClassDump();
+        long referencesCount = 0;
+        long nullReferencesCount = 0;
+
+        for (InstanceFieldDump field : classDump.getInstanceFields()) {
+            Object value = instance.getInstanceFieldValues().get(field);
+
+            if (value instanceof Value && ((Value<?>) value).type.equals(Type.OBJ) && ((Value<?>) value).value instanceof Long) {
+                Long reference = (Long) ((Value<?>) value).value;
+                if (reference != null) {
+                    if (0 == reference) {
+                        nullReferencesCount++;
+                    } else {
+                        referencesCount++;
+                    }
+                }
+            }
+        }
+
+        return new ReferenceCounter(referencesCount, nullReferencesCount, instance.getInstanceId());
+    }
+
+    private static class ReferenceCounter {
+        private final long referencesCount;
+        private final long nullReferencesCount;
+        private final long instanceId;
+
+        public ReferenceCounter(long referencesCount, long nullReferencesCount, long instanceId) {
+            this.referencesCount = referencesCount;
+            this.nullReferencesCount = nullReferencesCount;
+            this.instanceId = instanceId;
+        }
+
+        public long getReferencesCount() {
+            return referencesCount;
+        }
+
+        public long getNullReferencesCount() {
+            return nullReferencesCount;
+        }
+
+        public long getInstanceId() {
+            return instanceId;
+        }
+    }
+}
