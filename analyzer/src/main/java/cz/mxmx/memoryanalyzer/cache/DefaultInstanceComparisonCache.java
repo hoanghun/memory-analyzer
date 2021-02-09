e590261bea847c22eba0d3331dcaa9fb93f4ec67
Hung Hoang <hoanghun5@gmail.com>
2021-02-09 15:04:53 +0100

    Cache for instance comparison results.

---
+++ analyzer/src/main/java/cz/mxmx/memoryanalyzer/cache/DefaultInstanceComparisonCache.java
@@ -0,0 +1,34 @@
+package cz.mxmx.memoryanalyzer.cache;
+
+import cz.mxmx.memoryanalyzer.model.InstanceDump;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class DefaultInstanceComparisonCache implements InstanceComparisonCache {
+
+    private final Map<Long, Map<Long, Boolean>> cache = new HashMap<>();
+
+    @Override
+    public Optional<Boolean> instancesEqual(InstanceDump a, InstanceDump b) {
+        InstanceDump first = a.getInstanceId() < b.getInstanceId() ? a : b;
+        InstanceDump second = first == a ? b : a;
+
+        return Optional.ofNullable(cache.get(first.getInstanceId()))
+                .map(cachedComparison -> cachedComparison.get(second.getInstanceId()));
+    }
+
+    @Override
+    public void cacheComparisonResult(InstanceDump a, InstanceDump b, boolean comparisonResult) {
+        InstanceDump first = a.getInstanceId() < b.getInstanceId() ? a : b;
+        InstanceDump second = first == a ? b : a;
+
+        cacheResult(first.getInstanceId(), second.getInstanceId(), comparisonResult);
+    }
+
+    private void cacheResult(long instanceKey, long comparedTo, boolean comparisonResult) {
+        Map<Long, Boolean> cachedComparison = cache.computeIfAbsent(instanceKey, a -> new HashMap<>());
+        cachedComparison.put(comparedTo, comparisonResult);
+    }
+}
---
+++ analyzer/src/main/java/cz/mxmx/memoryanalyzer/cache/InstanceComparisonCache.java
@@ -0,0 +1,25 @@
+package cz.mxmx.memoryanalyzer.cache;
+
+import cz.mxmx.memoryanalyzer.model.InstanceDump;
+
+import java.util.Optional;
+
+public interface InstanceComparisonCache {
+
+    /**
+     * Returns comparison stored in the cache wrapped in Optional, if comparison has not been cached
+     * between two instances, returns Optional.Empty.
+     * @param a instance
+     * @param b instance
+     * @return Optional cached comparison result between two instance a and b
+     */
+    Optional<Boolean> instancesEqual(InstanceDump a, InstanceDump b);
+
+    /**
+     * Stores comparison between two instances into the cache.
+     * @param a instance
+     * @param b instance
+     * @param comparisonResult comparison result of these two instances
+     */
+    void cacheComparisonResult(InstanceDump a, InstanceDump b, boolean comparisonResult);
+}
