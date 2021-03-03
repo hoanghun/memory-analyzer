1710abf4cdc4c01e5cdfcccb7e30ff4eb9ff4639
Hung Hoang <hoanghun5@gmail.com>
2021-03-03 16:48:17 +0100

    Return of the shallow equals.

--- analyzer/src/main/java/cz/mxmx/memoryanalyzer/memorywaste/DuplicateInstanceWasteAnalyzer.java
+++ analyzer/src/main/java/cz/mxmx/memoryanalyzer/memorywaste/DuplicateInstanceWasteAnalyzer.java
@@ -1,9 +1,5 @@
 package cz.mxmx.memoryanalyzer.memorywaste;
 
-import cz.mxmx.memoryanalyzer.cache.DefaultInstanceCacheWithStatistics;
-import cz.mxmx.memoryanalyzer.cache.DummyInstanceComparisonCache;
-import cz.mxmx.memoryanalyzer.cache.InstanceComparisonCache;
-import cz.mxmx.memoryanalyzer.cache.InstanceComparisonCacheWithStatistics;
 import cz.mxmx.memoryanalyzer.model.ClassDump;
 import cz.mxmx.memoryanalyzer.model.InstanceDump;
 import cz.mxmx.memoryanalyzer.model.InstanceFieldDump;
@@ -21,21 +17,20 @@ import java.util.*;
  */
 public class DuplicateInstanceWasteAnalyzer implements WasteAnalyzer {
     private static final Logger log = LoggerFactory.getLogger(DuplicateInstanceWasteAnalyzer.class);
-    private final Set<InstancesIds> currentlyComparing = new HashSet<>();
-    private final InstanceComparisonCacheWithStatistics cache;
+    private boolean useDeepEquals = true;
 
     public DuplicateInstanceWasteAnalyzer() {
-        this.cache = new DefaultInstanceCacheWithStatistics(new DummyInstanceComparisonCache());
     }
 
-    public DuplicateInstanceWasteAnalyzer(InstanceComparisonCache cache) {
-        this.cache = new DefaultInstanceCacheWithStatistics(cache);
+    public DuplicateInstanceWasteAnalyzer(boolean useDeepEquals) {
+        this.useDeepEquals = useDeepEquals;
     }
 
     /**
      * Analyzes specified instances and finds duplicate instances.
      * Instances a,b are duplicate iff their fields are the same
      * including field references.
+     *
      * @param memoryDump Memory dump to process.
      * @return list of instances and their duplicates.
      */
@@ -49,10 +44,10 @@ public class DuplicateInstanceWasteAnalyzer implements WasteAnalyzer {
         int onePercent = total / 100;
         log.info("Starting duplicate instance waste analyzer. Using deep equals.");
         long doneCount = 0;
-        List<InstanceDump> duplicates = new ArrayList<>();
+        List<InstanceDump> duplicates;
 
         for (int i = 0; i < instances.size() - 1; i++) {
-            duplicates.clear();
+            duplicates = new ArrayList<>();
             InstanceDump instance = instances.get(i);
             if (instance == null) continue;
             for (int j = i + 1; j < instances.size(); j++) {
@@ -87,16 +82,21 @@ public class DuplicateInstanceWasteAnalyzer implements WasteAnalyzer {
      * @return True if the instances are the same, otherwise false.
      */
     public boolean instancesAreSame(InstanceDump instance, InstanceDump instance2) {
-        currentlyComparing.clear();
-        return deepEquals(instance, instance2, currentlyComparing);
+        if (useDeepEquals) {
+            final Set<InstancesIds> currentlyComparing = new HashSet<>();
+            return deepEquals(instance, instance2, currentlyComparing);
+        } else {
+            return shallowEquals(instance, instance2);
+        }
     }
 
     /**
      * Compares instances a and b based on their fields. If their fields are references, these
      * fields are recursively compared too. Instances have to be of the same class hierarchy
      * and have to have same amount of fields to even consider that they are duplicates.
-     * @param a instance A
-     * @param b instance B
+     *
+     * @param a                  instance A
+     * @param b                  instance B
      * @param currentlyComparing set of currently comparing pairs to prevent cycles.
      * @return true if a and b are duplicates else false
      */
@@ -135,6 +135,60 @@ public class DuplicateInstanceWasteAnalyzer implements WasteAnalyzer {
         instancesSame = instancesSame && classDump.getInstanceFields().size() > 0;
 
         return instancesSame;
+    }
+
+    private boolean instancesOfSameClass(InstanceDump instance, InstanceDump instance2) {
+        ClassDump parent = instance.getClassDump();
+
+        do {
+            if (parent.equals(instance2.getClassDump())) {
+                return true;
+            }
+
+            parent = parent.getSuperClassDump();
+        } while (parent != null && parent.getName() != null && !parent.getName().equals(Object.class.getName()));
+
+        parent = instance2.getClassDump();
+
+        do {
+            if (parent.equals(instance.getClassDump())) {
+                return true;
+            }
+
+            parent = parent.getSuperClassDump();
+        } while (parent != null && parent.getName() != null && !parent.getName().equals(Object.class.getName()));
+
+        return false;
+    }
+
+    /**
+     * @param a instance dump a
+     * @param b instance dumb b
+     * @return true if objects have same references or string values.
+     */
+    @SuppressWarnings("unused")
+    private boolean shallowEquals(InstanceDump a, InstanceDump b) {
+        if (a.getInstanceFieldValues().size() != b.getInstanceFieldValues().size()) {
+            return false;
+        }
+
+        if (this.instancesOfSameClass(a, b)) {
+            ClassDump classDump = a.getClassDump();
+
+            for (InstanceFieldDump<?> field : classDump.getInstanceFields()) {
+                Object value = a.getInstanceFieldValues().get(field);
+                Object value2 = b.getInstanceFieldValues().get(field);
+
+                if (!value.equals(value2)) {
+                    return false;
+                }
+            }
+            return classDump.getInstanceFields().size() > 0;
+        }
+
+        return false;
+
+
     }
 
     private static class InstancesIds {
--- analyzer/src/main/java/cz/mxmx/memoryanalyzer/model/InstanceFieldDump.java
+++ analyzer/src/main/java/cz/mxmx/memoryanalyzer/model/InstanceFieldDump.java
@@ -2,6 +2,8 @@ package cz.mxmx.memoryanalyzer.model;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
+import java.util.Objects;
+
 /**
  * Instance field.
  * @param <T> Type of the field.
@@ -34,5 +36,18 @@ public class InstanceFieldDump<T> {
 				.append("name", name)
 				.append("type", type)
 				.toString();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		InstanceFieldDump<?> that = (InstanceFieldDump<?>) o;
+		return Objects.equals(name, that.name) && Objects.equals(type, that.type);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, type);
 	}
 }
